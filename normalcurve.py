cc71a66d0b535d8450875a3dcadeb50251a170d8
alex-he <mr.a.he98@gmail.com>
2022-12-05 16:04:26 +1000

    Layer on normal curves.

--- normalcurve.py
+++ normalcurve.py
@@ -18,6 +18,9 @@ class NormalCurve:
             raise ValueError( "Wrong number of edge weights!" )
 
         self._tri = tri
+        self._processWeights(weights)
+
+    def _processWeights( self, weights ):
         self._weights = tuple(weights)
 
         # Check that the given weights satisfy the matching constraints.
@@ -35,7 +38,7 @@ class NormalCurve:
         # - assign an index to each component of this normal curve; and
         # - record precisely how this normal curve traverses the boundary of
         #   our triangulation.
-        self._arcCoords = [None] * tri.countTriangles()
+        self._arcCoords = [None] * self._tri.countTriangles()
         self._roots = list( self._points )
         self._adjPoints = { p: list() for p in self._points }
         self._adjCutVerts = { p: list() for p in self._points }
@@ -189,6 +192,13 @@ class NormalCurve:
                 self._switch[p] = (
                         self._adjCutVerts[p][0] != self._adjCutVerts[p][1] )
 
+    def triangulation(self):
+        """
+        Returns a copy of the triangulation whose boundary contains this
+        normal curve.
+        """
+        return Triangulation3( self._tri )
+
     def countComponents(self):
         """
         Returns the number of components of this normal curve.
@@ -220,6 +230,73 @@ class NormalCurve:
                 total += 1
         return total
 
+    def _flipWeight( self, e ):
+        # Compute the new weight after flipping the edge e.
+        # Start by counting "non-corner arcs" that meet e, as these will also
+        # meet the new edge.
+        newWeight = 0
+        ind = e.index()
+        wt = self._weights[ind]
+        for i in range(wt):
+            if self._switch[ (ind, i) ]:
+                # Switch implies non-corner.
+                newWeight += 1
+
+        # Now count "corner arcs" that will meet the new edge.
+        emb = [ e.embedding(0), e.embedding( e.degree() - 1 ) ]
+        for i in range(2):
+            tet = emb[i].tetrahedron()
+            verts = emb[i].vertices()
+            otherArcs = 0
+            for j in range(2):
+                otherInd = tet.edge( verts[j], verts[2+i] ).index()
+                otherArcs += self._weights[otherInd]
+            otherArcs = ( otherArcs - wt ) // 2
+            newWeight += otherArcs
+
+        return newWeight
+
+    def layerOn( self, e ):
+        """
+        Layer a new tetrahedron across the edge e.
+
+        Pre-condition:
+        --> e is a boundary edge of self.triangulation().
+        """
+        temp = []
+        for edge in self._tri.edges():
+            if edge == e:
+                continue
+            emb = edge.embedding(0)
+            temp.append(
+                    ( emb.tetrahedron(), emb.vertices(),
+                        self._weights[ edge.index() ] ) )
+        flipWeight = self._flipWeight(e)
+        #TODO TEST
+        print( "Flip weight: {}.".format(flipWeight) )
+
+        # Layer on new tetrahedron.
+        newTet = self._tri.layerOn(e)
+        temp.append( ( newTet, Perm4(2,3,0,1), flipWeight ) )
+
+        # Use temp to compute new weight coordinates.
+        newWeights = [0] * self._tri.countEdges()
+        for tet, verts, wt in temp:
+            edgeInd = tet.edge( verts[0], verts[1] ).index()
+            newWeights[edgeInd] = wt
+        self._processWeights(newWeights)
+
+    def flipEdge( self, e ):
+        """
+        Flip the edge e by either removing a layered tetrahedron, or layering
+        a new tetrahedron across this edge.
+
+        Pre-condition:
+        --> e is a boundary edge of self.triangulation().
+        """
+        #TODO
+        pass
+
     #TODO What's the best way to provide user access to arc coordinates?
     #TODO
     pass
@@ -227,14 +304,19 @@ class NormalCurve:
 
 # Test code.
 if __name__ == "__main__":
-    tri = Triangulation3.fromIsoSig( "eHbecadjk" )
+    initTri = Triangulation3.fromIsoSig( "eHbecadjk" )
     msg = "{} components. Switches: {}."
     testCases = [
             (0,0,0,2,2,2,3,1,1),    # 1-component
             (1,0,1,2,3,2,3,2,1),    # 2-component
             (3,5,4,2,3,5,4,5,3) ]   # 3-component
     for w in testCases:
+        tri = Triangulation3(initTri)
         curve = NormalCurve( tri, w )
         comps = curve.countComponents()
         print( msg.format( comps,
             [ curve.countSwitchesInComponent(i) for i in range(comps) ] ) )
+        curve.layerOn( tri.edge(0) )
+        print( msg.format( comps,
+            [ curve.countSwitchesInComponent(i) for i in range(comps) ] ) )
+        print()
